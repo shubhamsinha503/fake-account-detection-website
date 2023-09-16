07eba6315b3ef8238d3e60c46d77b78ff107169b
shubhamsinha503 <94913494+shubhamsinha503@users.noreply.github.com>
2023-09-16 06:18:54 +0000

    new login page created

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Dummy user database (replace with a real database)
+users = {'shubham': '123456', 'user2@example.com': 'password2'}
+
 
 @app.route("/")
-def hello_world():
-  return "Hello, World"
+def home():
+  return render_template('home.html')
+
+
+@app.route("/login", methods=['POST'])
+def login():
+  name = request.form['txtN']
+
+  
+  if name == "xyz":
+    return render_template('logoutpage.html')
+  else:
+    return render_template('loginpage.html')
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ database.py
@@ -0,0 +1,4 @@
+from replit import db
+
+# Add a key and associated value to the database
+db["key1"] = "value1"
